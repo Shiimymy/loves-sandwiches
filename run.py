c9c2e21d9238bc20d5c400a087665f8c8021d2d3
Shiimymy <>
2023-08-09 12:25:47 +0000

    Collect sales data for last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     Get sales figures input from user
@@ -51,7 +52,7 @@ def validate_data(values):
     return True
 
 
-#def update_sales_worksheet(data):
+# def update_sales_worksheet(data):
 #    """
 #    Update sales worksheet, add new row with the list data provided.
 #    """
@@ -61,7 +62,7 @@ def validate_data(values):
 #    print("Sales worksheet updated successfully. \n")
 
 
-#def update_surplus_worksheet(data):
+# def update_surplus_worksheet(data):
 #    """
 #    Update surplus worksheet, add new row with the list data provided.
 #    """
@@ -102,6 +103,21 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Collects collums of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
+
 def main():
     """
     Run all program function
@@ -114,4 +130,6 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
